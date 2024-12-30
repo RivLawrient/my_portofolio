fbdaf0716476e5fbac9c1b940e5af86ca4576621
lawrient <sandincl.d@gmail.com>
2024-12-31 01:14:49 +0800

    FEAT: text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <button className="text-[90px] active:bg-black/50 font-extrabold leading-none m-4 bg-black text-pink-500 p-3 rounded-2xl">
+      Black Pink
+    </button>
+  );
 }
