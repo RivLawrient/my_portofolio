02afae323430d2ad5dfb2360ea8aa09979619a30
lawrient <sandincl.d@gmail.com>
2024-12-31 01:43:49 +0800

    FEAT: text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <button className="text-[90px] active:bg-black/50 font-extrabold leading-none m-4 bg-black text-pink-500 p-3 rounded-2xl">
-      Black Pink
+      Muh Sandi
     </button>
   );
 }
