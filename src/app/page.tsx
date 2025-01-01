d1b19c474e6464c38514c85b0c0780381e2cec05
lawrient <sandincl.d@gmail.com>
2025-01-02 07:15:06 +0800

    FEAT: welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,57 @@ import { useEffect, useState } from "react";
 export default function Page() {
   return (
     <>
-      <section className="w-screen h-screen items-center justify-center flex  from-60% via-40% from-gray-900 to-indigo-950 bg-gradient-to-b">
-        <Header />
-        <Welcome text="hello, welcome" />
+      <Welcome text="hello, welcome" />
+      <Header />
+      <section className="w-screen h-screen  items-center justify-center flex flex-col bg-gray-900/90">
+        <img
+          src="./fll.jpg"
+          alt=""
+          className="absolute w-screen -z-10 blur-md "
+        />
+        <CardIntro />
       </section>
-      <section className="w-screen h-screen"></section>
+      <section className="w-screen h-screen bg-gray-900"></section>
+      <section className="w-screen h-screen bg-gray-900"></section>
     </>
   );
 }
+function CardIntro() {
+  const [hidden, setHidden] = useState<boolean>(true);
+  useEffect(() => {
+    setInterval(() => {
+      setHidden(false);
+    }, 5000);
+  }, []);
 
+  return (
+    <div
+      className={`${
+        hidden
+          ? "md:h-[200px] h-[140px]"
+          : "md:h-[450px] h-[60%] transition-all"
+      } bg-gray-800/30 border border-gray-800 rounded-2xl md:w-[800px] w-[480px] relative  flex justify-center`}
+    >
+      <div
+        className={`${
+          hidden ? "opacity-0" : "opacity-1 transition-all"
+        } absolute top-[-75px] font-mono leading-none flex flex-col text-gray-500 text-start`}
+      >
+        <span className="text-[42px] ">
+          Hi, I'm <span className="font-semibold">Sandi</span>
+        </span>
+        <span className="my-2">fullstack web / backend developer</span>
+      </div>
+      <img
+        src="./mypicture.png"
+        alt=""
+        className={`${
+          hidden ? "opacity-0" : "opacity-1 transition-all"
+        } object-cover md:ml-[-60px] md:h-[400px] h-[300px] w-[200px] md:w-[300px] absolute left-0 bottom-0 rounded-2xl `}
+      />
+    </div>
+  );
+}
 function Welcome(props: { text: string }) {
   const [hidden, setHidden] = useState<boolean>(false);
   const [text, setText] = useState<string>("");
@@ -37,22 +79,24 @@ function Welcome(props: { text: string }) {
       for (let index = props.text.length - 1; index >= 0; index--) {
         setTimeout(() => {
           setText((prev) => prev.slice(0, prev.length - 1));
-        }, 200 * (props.text.length - index));
+        }, 100 * (props.text.length - index));
       }
 
       setTimeout(() => {
         setIsReversing(false);
         setHidden(true);
-      }, props.text.length * 200 + 1000);
+      }, props.text.length * 100 + 1000);
     }
   }, [isReversing, props.text]);
 
   if (hidden) return null;
   return (
-    <div className="md:text-[100px] text-[80px] w-max text-gray-500 ">
-      <span className="tracking-widest border-r-[4px] transition-[5s] animate-cursortext border-gray-500">
-        {text}
-      </span>
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+      <div className="md:text-[100px] text-[48px] w-max text-gray-500 ">
+        <span className="tracking-widest border-r-[4px] transition-[5s] animate-cursortext border-gray-500">
+          {text}
+        </span>
+      </div>
     </div>
   );
 }
